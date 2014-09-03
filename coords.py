1b99502042a06aea50f8c11a23afc26dc00ffda0
Samuel Russell <sr228822@gmail.com>
2014-09-03 10:05:51 -0700

    Add a coordinate helper library (found, not written)

---
+++ coords.py
@@ -0,0 +1,35 @@
+import math
+
+KM_PER_EARTH_RADIUS = 6373
+
+def distkm(latlng1, latlng2):
+    lat1, lng1 = latlng1
+    lat2, lng2 = latlng2
+
+    # Convert latitude and lngitude to 
+    # spherical coordinates in radians.
+    degrees_to_radians = math.pi/180.0
+        
+    # phi = 90 - latitude
+    phi1 = (90.0 - lat1)*degrees_to_radians
+    phi2 = (90.0 - lat2)*degrees_to_radians
+        
+    # theta = lngitude
+    theta1 = lng1*degrees_to_radians
+    theta2 = lng2*degrees_to_radians
+        
+    # Compute spherical distance from spherical coordinates.
+        
+    # For two locations in spherical coordinates 
+    # (1, theta, phi) and (1, theta, phi)
+    # cosine( arc length ) = 
+    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    # distance = rho * arc length
+    
+    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
+           math.cos(phi1)*math.cos(phi2))
+    arc = math.acos( cos )
+
+    # Remember to multiply arc by the radius of the earth 
+    # in your favorite set of units to get length.
+    return (arc * KM_PER_EARTH_RADIUS)
